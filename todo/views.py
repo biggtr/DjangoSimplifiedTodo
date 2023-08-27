40455d383279a97a7fda368da83bb81f7ff2c07a
bigtorresg <bigtorresg@gmail.com>
2023-08-27 17:09:23 +0300

    urls added

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def TaskListView(request):
+    pass
+
+
+def TaskCreateView(request):
+    pass
+
+
+def TaskUpdateView(request, task_id):
+    pass
+
+
+def TaskDeleteView(request, task_id):
+    pass
