a71f6f9ca2b563aaef50c4236f2009b778f65119
bigtorresg <bigtorresg@gmail.com>
2023-08-27 18:45:16 +0300

    crud Added

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import get_list_or_404, get_object_or_404, render
-
+from django.shortcuts import get_list_or_404, get_object_or_404, redirect, render
+from .forms import TaskForm
 from .models import Task
 
 # Create your views here.
@@ -11,12 +11,31 @@ def TaskListView(request):
 
 
 def TaskCreateView(request):
-    pass
+    form = TaskForm(request.POST)
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            return redirect("task-list")
+    else:
+        form = TaskForm()
+    return render(request, "task_create.html", context={"form": form})
 
 
 def TaskUpdateView(request, task_id):
-    pass
+    task = get_object_or_404(Task, pk=task_id)
+    if request.method == "POST":
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect("task-list")
+    else:
+        form = TaskForm(instance=task)
+    return render(request, "task_update.html", context={"form": form})
 
 
 def TaskDeleteView(request, task_id):
-    pass
+    task = get_object_or_404(Task, pk=task_id)
+    if request.method == "POST":
+        task.delete()
+        return redirect("task-list")
+    return render(request, "task_delete.html", context={"task": task})
