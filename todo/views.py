ab94eed85f0ca6ba96c2432400f4df24fc01479d
bigtorresg <bigtorresg@gmail.com>
2023-08-27 17:46:36 +0300

    list view added

--- todo/views.py
+++ todo/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, get_object_or_404, render
+
+from .models import Task
 
 # Create your views here.
 
 
 def TaskListView(request):
-    pass
+    tasks = get_list_or_404(Task)
+    return render(request, "tasks_list.html", context={"tasks": tasks})
 
 
 def TaskCreateView(request):
